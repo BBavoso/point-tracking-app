c1d57396ad41d64cb22e69a49316a83e99f7234c
BBavoso <bmbavoso@gmail.com>
2023-08-18 02:27:53 -0400

    it's a start for sure

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import React from "react";
 import "./App.css";
 
+function PointBox() {
+  return (
+    <div className="pointBox">
+      <div className="pointBox-checkBox"></div>
+      <h1>points: </h1>
+    </div>
+  );
+}
+
 class App extends React.Component {
   constructor() {
     super();
@@ -14,6 +23,7 @@ class App extends React.Component {
           hello! <br />
           pretty cool
         </h1>
+        <PointBox points={2} />
       </div>
     );
   }
